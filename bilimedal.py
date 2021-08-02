df7bfe7e5b820560a553851df26616c9e47c98bc
jjnnnh <37836145+jjnnnh@users.noreply.github.com>
2021-08-02 17:05:57 +0800

    优化爬取过程
    无需再访问粉丝榜获取粉丝勋章，粉丝勋章为粉丝团写入到"medalfst.txt"

--- bilimedal.py
+++ bilimedal.py
@@ -17,34 +17,19 @@ def getHTML(url, params=None):  # 爬取网页，获得网页HTML
         return ''  # 异常处理
 
 
-def getRuid(roomid):    # 获取主播b站id
+def getMedal(roomid):    # 直接获取勋章
     try:
         url = 'https://live.bilibili.com/' + str(roomid)  # 直播间url
         html = getHTML(url)  # 获得直播间HTML
-        uid = regex_ruid.search(html).group()  # 通过正则表达式匹配直播间HTML，获取主播id
-        return uid  # 返回主播id
+        match = regex_medal.findall(html)  # 通过正则表达式匹配查询结果，获取粉丝勋章名称
+        if len(match):  # 如果该直播间开通了粉丝勋章且粉丝榜有粉丝
+            return match[0]  # 返回获取粉丝勋章名称
+        else:
+            return ''  # 否则返回空字符串
     except:
         return AssertionError  # 异常处理
 
-
-def getMedal(roomid, ruid):  # 获取粉丝勋章
-    try:
-        url = 'https://api.live.bilibili.com/rankdb/v1/RoomRank/webMedalRank'  # 查询粉丝勋章的网页api
-        params = {'roomid': roomid, 'ruid': ruid}  # 需要房间号和主播b站id作为参数
-        medalinfo = getHTML(url, params=params)  # 返回查询结果
-        match = regex_medal.findall(medalinfo)  # 通过正则表达式匹配查询结果，获取粉丝勋章名称
-        if len(match) > 1:  # 如果该直播间开通了粉丝勋章且粉丝榜有粉丝
-            return match[1]  # 返回获取粉丝勋章名称
-        else:
-            return ''  # 否则返回空字符串
-    except:  # 异常处理
-        with open('medal.txt', 'a', encoding='utf-8') as medalfile:
-            # 在"medal.txt"中添加错误记录
-            print('房间' + str(roomid) + '爬取失败', file=medalfile)
-
-
 # 程序入口
-regex_ruid = re.compile(r'(?<="uid":)\d*')  # 用于匹配主播id的正则表达式
 regex_medal = re.compile(r'(?<="medal_name":")[^"]*')  # 用于匹配粉丝勋章的正则表达式
 with open('settings.json', 'r') as f:  # 读取配置文件
     settings = json.load(f)
@@ -57,13 +42,22 @@ with open('log.txt', 'w', encoding='utf-8') as logfile:  # 清空"log.txt"
     print('爬取开始', file=logfile)
 with open('medal.txt', 'w', encoding='utf-8') as medalfile:  # 清空"medal.txt"
     print('勋章字典：', file=medalfile)
+with open('medalfst.txt', 'w', encoding='utf-8') as medalfstfile:  # 清空"fst.txt"
+    print('勋章粉丝团：', file=medalfstfile)
 for i in range(num1, num2):  # 遍历直播间
     # 先调用getRuid()函数获取该直播间主播id，再用房间号和主播id为参数调用getMedal()获取粉丝勋章
-    medalname = getMedal(i, getRuid(i))
-    if medalname:  # 如果该直播间开通了粉丝勋章且粉丝榜有粉丝
-        with open('medal.txt', 'a', encoding='utf-8') as medalfile:  # 以追加模式打开"medal.txt"，注意编码格式
-            print('{}:{}'.format(medalname, i),
-                  file=medalfile)  # 写入粉丝勋章及其对应的房间号
+    medalname = getMedal(i)
+    if medalname:  # 如果该直播间开通了粉丝勋章
+        # 判断默认粉丝勋章"粉丝团"
+        if medalname == "粉丝团":
+            WJ = "medalfst.txt"
+            XR = i
+        else:
+            WJ = "medal.txt"
+            XR = '{}:{}'.format(medalname, i)
+        with open(WJ, 'a', encoding='utf-8') as medalfile:  # 以追加模式按需打开文件，注意编码格式
+            print(XR,
+                  file=medalfile)  # 按需写入
     if i % num_percent == 0:  # 显示进度
         with open('log.txt', 'w', encoding='utf-8') as logfile:  # 以写入模式打开"log.txt"
             print('{} {}/{} {:.2f}% compelete'.format(time.ctime(),
